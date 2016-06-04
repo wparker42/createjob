ae5a523822e8aae7b29092267a874817d9345283
wparker42 <williamparker.ty@gmail.com>
2016-06-04 09:52:18 -0600

    Progress on Tkinter gui

--- run.py
+++ run.py
@@ -1,5 +1,49 @@
 import os
 import sys
+import tkinter as tk
+
+class CreateJobApp(tk.Tk):
+    """docstring for CreateJobApp"""
+    def __init__(self):
+        tk.Tk.__init__(self)
+
+        departments = [
+            ('Civil',1),
+            ('Environmental',2),
+            ('Structural',3)
+        ]
+
+        deptvar = tk.IntVar()
+
+        self.wm_title("Create Job")
+
+        tk.Label(
+            self, 
+            text="Select Department",
+            padx=20
+        ).grid(row=0,column=0)
+
+        tk.Label(
+            self,
+            text="Job Number"
+        ).grid(row=0,column=3)
+
+        self.jobnum = tk.Entry(self).grid(row=1,column=3)
+
+        r = 1
+        for txt, val in departments:
+            self.departselect = tk.Radiobutton(
+                self,
+                indicatoron=0,
+                text=txt,
+                width=20,
+                padx=20, 
+                variable=deptvar,
+                value=val).grid(row=r,column=0)
+            r+=1
+
+w = CreateJobApp()
+w.mainloop()
 
 
 def generatefile():
