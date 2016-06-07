fc33771cd2754f78309d5f23b42fd68a94c22324
wparker42 <williamparker.ty@gmail.com>
2016-06-07 08:14:00 -0600

    Source paths for JVA

--- mastertemplates.py
+++ mastertemplates.py
@@ -1,5 +1,5 @@
 templatepathdict = {
-    1:('Civil','directories/templates/civ/0000c/'),
-    2:('Environmental','directories/templates/env/9999c/'),
-    3:('Structural','directories/templates/env/9999c/')
+    1:('Civil', r'\\bldr-civil1\bldr_civil\CIVIL\NJFT\_NJFT'),
+    2:('Environmental', r'\\bldr-civil1\Bldr_Civil\CIVIL\Environmental\2015 Efficiency - Team 5\Standard Job Folder Organization\9999c'),
+    3:('Structural', r'\\bldr-struct1\bldr_structural\_proposed folder tree')
 }
