6bac073c75984a65cfc2df3df01339d7d9bef171
wparker42 <typarker123@gmail.com>
2018-03-26 08:34:40 -0600

    Changed template source directory

--- mastertemplates.py
+++ mastertemplates.py
@@ -1,7 +1,7 @@
 templatepathdict = {
-    1:('Civil', r'\\bldr-civil1\bldr_civil\CIVIL\NJFT\_NJFT'),
-    2:('Environmental', r'\\bldr-civil1\Bldr_Civil\CIVIL\Environmental\2015 Efficiency - Team 5\Standard Job Folder Organization\9999c'),
-    3:('Structural', r'\\bldr-struct1\bldr_structural\_proposed folder tree')
+    1:('Civil', r'\\administration1\JVA_Resource\Job Folder Templates\Civil'),
+    2:('Environmental', r'\\administration1\JVA_Resource\Job Folder Templates\Environmental'),
+    3:('Structural', r'\\administration1\JVA_Resource\Job Folder Templates\Structural')
 }
 
 serverlistdict = {
